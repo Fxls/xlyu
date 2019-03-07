bf03a6662ef610a8dc23c5391168069eb729f16c
Administrator <1163055423@qq.com>
2019-03-07 09:29:59 +0800

    day05的作业提交

---
+++ src/day5/ArrInitialization.java
@@ -0,0 +1,18 @@
+/**
+ * @Author xlyu
+ * @Date 2019/3/6 0006
+ * @Description
+ */
+package day5;
+
+/**
+ * @program: homeworkday04
+ * @description: 初始化数组方法1
+ * @author: xlyu
+ * @create: 2019-03-06 22:18
+ **/
+public class ArrInitialization {
+    public static void main(String[] args) {
+        int[] arr = new int[4];
+    }
+}
---
+++ src/day5/ArrInitialization2.java
@@ -0,0 +1,19 @@
+/**
+ * @Author xlyu
+ * @Date 2019/3/6 0006
+ * @Description
+ */
+package day5;
+
+/**
+ * @program: homeworkday04
+ * @description: 初始化数组方法2
+ * @author: xlyu
+ * @create: 2019-03-06 22:18
+ **/
+public class ArrInitialization2 {
+    public static void main(String[] args) {
+        int[] arr = new int[]{0, 0, 0, 0,};
+
+    }
+}
