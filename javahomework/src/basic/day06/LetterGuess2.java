868bfbc7dd020cb9abddf0f30f3e8841804cf032
Administrator <1163055423@qq.com>
2019-03-08 19:45:07 +0800

    作业提交

--- javahomework/src/basic/day06/LetterGuess2.java
+++ javahomework/src/basic/day06/LetterGuess2.java
@@ -7,9 +7,14 @@ package basic.day06;
 
 /**
  * @program: javahomework
- * @description:
+ * @description: 猜字母游戏v2.0(高级版本)：用户指定游戏难度（5、7、9），
+ * 程序生成随机不重复字母。用户猜错，提示用户对的字母数量和对的位置数量。
  * @author: xlyu
  * @create: 2019-03-08 18:50
  **/
 public class LetterGuess2 {
+    public static void main(String[] args) {
+        //随机生成5个字符的数组，
+
+    }
 }
