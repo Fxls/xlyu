42e9c7d5e5d81e38e02e83f53778754dd387e0c2
余祥磊 <xlyu@gitblit>
2019-05-25 13:03:23 +0800

    [servletDay03&新增功能：修改]

---
+++ servletDay03/src/com/itek/servlet/day03/dao/inter/AccountInter.java
@@ -0,0 +1,52 @@
+package com.itek.servlet.day03.dao.inter;
+
+import com.itek.servlet.day03.module.Account;
+
+import java.util.List;
+
+/**
+ * @Param
+ * @description Account表的接口，定义了如下方法
+ * @date 2019/5/24 18:50
+ * @return
+ */
+
+public interface AccountInter {
+
+    /**
+     * @Param
+     * @description 数据库增加数据功能
+     * @date 2019/5/24 18:51
+     * @return
+     */
+
+    int addAccount(Account account);
+
+    /**
+     * @Param
+     * @description 数据库删除字段功能
+     * @date 2019/5/24 18:51
+     * @return
+     */
+
+    int deleteAccountById(int id);
+
+    /**
+     * @Param
+     * @description 数据库查询功能
+     * @date 2019/5/24 18:51
+     * @return
+     */
+
+    List<Account> selectAccount();
+
+    /**
+     * @Param
+     * @description 数据库更改数据功能
+     * @date 2019/5/24 18:52
+     * @return
+     */
+
+    int updateAccountById(Account account,int id);
+
+}
